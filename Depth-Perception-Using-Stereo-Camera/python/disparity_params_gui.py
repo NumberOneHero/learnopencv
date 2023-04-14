cfbdc41f307c5bdfb0fadf262409cab4209b3eb2
NumberOneHero <wwwru98@live.com.pt>
2023-04-14 18:10:27 +0100

    updated for better frames
    hopefully

--- Depth-Perception-Using-Stereo-Camera/python/disparity_params_gui.py
+++ Depth-Perception-Using-Stereo-Camera/python/disparity_params_gui.py
@@ -24,28 +24,34 @@ ret = None
 imgR = None
 imgL = None
 def Esp32Frame(stream,img,bts,ret):
+	jpghead = -1
+	jgpend = -1
+	while (jpghead < 0 or jgpend < 0):
+		bts += stream.read()
 
-	bts += stream.read(CAMERA_BUFFRER_SIZE)
-	jpghead = bts.find(b'\xff\xd8')
-	jpgend = bts.find(b'\xff\xd9')
+		if jpghead < 0 :
+			jpghead = bts.find(b'\xff\xd8')
+		if jgpend < 0:
 
-
-	if jpghead > -1 and jpgend > -1:
-		jpg = bts[jpghead:jpgend + 2]
-		bts = bts[jpgend + 2:]
-
-		img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
-		img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-			# h,w=img.shape[:2]
-			# print('影像大小 高:' + str(h) + '寬：' + str(w))
-			# img2 = img
-
-		k = cv2.waitKey(5)
-		ret = True
+			jpgend = bts.find(b'\xff\xd9')
 
 
-	else:
-		ret= False
+		if jpghead > -1 and jpgend > -1:
+			jpg = bts[jpghead:jpgend + 2]
+			bts = bts[jpgend + 2:]
+
+			img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_UNCHANGED)
+			img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+				# h,w=img.shape[:2]
+				# print('影像大小 高:' + str(h) + '寬：' + str(w))
+				# img2 = img
+
+			k = cv2.waitKey(5)
+			ret = True
+
+
+		else:
+			ret= False
 
 
 
@@ -110,7 +116,7 @@ while True:
 							cv2.INTER_LANCZOS4,
 							cv2.BORDER_CONSTANT,
 							0)
-		
+
 		# Applying stereo image rectification on the right image
 		Right_nice= cv2.remap(imgR_gray,
 							Right_Stereo_Map_x,
@@ -131,7 +137,7 @@ while True:
 		speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize','disp')*2
 		disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff','disp')
 		minDisparity = cv2.getTrackbarPos('minDisparity','disp')
-		
+
 		# Setting the updated parameters before computing disparity map
 		stereo.setNumDisparities(numDisparities)
 		stereo.setBlockSize(blockSize)
@@ -148,13 +154,13 @@ while True:
 		# Calculating disparity using the StereoBM algorithm
 		disparity = stereo.compute(Left_nice,Right_nice)
 		# NOTE: compute returns a 16bit signed single channel image,
-		# CV_16S containing a disparity map scaled by 16. Hence it 
+		# CV_16S containing a disparity map scaled by 16. Hence it
 		# is essential to convert it to CV_32F and scale it down 16 times.
 
-		# Converting to float32 
+		# Converting to float32
 		disparity = disparity.astype(np.float32)
 
-		# Scaling down the disparity values and normalizing them 
+		# Scaling down the disparity values and normalizing them
 		disparity = (disparity/16.0 - minDisparity)/numDisparities
 
 		# Displaying the disparity map
@@ -164,7 +170,7 @@ while True:
 		# Close window using esc key
 		if cv2.waitKey(1) == 27:
 			break
-	
+
 	else:
 		btsL,imgL,retL = Esp32Frame(streamLeft, imgL,btsL,retL)
 		btsR,imgR,retR = Esp32Frame(streamRight, imgR,btsR,retR)
